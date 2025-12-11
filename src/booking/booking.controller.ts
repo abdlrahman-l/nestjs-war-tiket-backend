30ada89887d5b99755820f5a52ff83703822379f
abdlrahman-l <abdulrahmaninformatics@gmail.com>
2025-12-11 10:57:03 +0700

    add guards and response message to create booking method

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
 import { GetUser } from 'src/auth/get-user.decorator';
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('booking')
 export class BookingController {
   constructor(private readonly bookingService: BookingService) {}
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
+  @ResponseMessage('Kursi berhasil diamankan! Segera bayar dalam 5 menit.')
   create(
     @Body() createBookingDto: CreateBookingDto,
     @GetUser('userId') userId: string,
